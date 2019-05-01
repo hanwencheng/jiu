e41f4f32b594f796e21cab1be97f2aed5d2d8cf8
hanwencheng <heawen.cheng@gmail.com>
2019-05-01 19:35:28 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['universe/web', "plugin:react/recommended"],
+  parserOptions: {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+  },
+  settings: {
+    react: {
+      "pragma": "React",  // Pragma to use, default to "React"
+      "version": "16.8.0", // React version, default to the latest React stable release
+    },
+  },
+  rules: {
+    "react/prefer-stateless-function": [
+      0, // disable
+      // { "ignorePureComponents": true }
+    ],
+    "handle-callback-err": 0
+  }
+};
