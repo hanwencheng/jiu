2171a9141cbf034b6dbe4d0338988d232871d2e9
hanwencheng <heawen.cheng@gmail.com>
2019-06-27 00:49:02 +0200

    add server CORS
    This reverts commit 0ee4695f308916e4cb5d25966e1540b44545ccae.

--- server/index.js
+++ server/index.js
@@ -28,6 +28,16 @@ const writeJsonFile = (jsonObj, resolve, reject) => {
     resolve('JSON file has been saved.')
   })
 }
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
